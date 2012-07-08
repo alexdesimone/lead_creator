74f223c5317a38c94534c9be11cf3c1cbfa0cf13
Alex De Simone <ajdesimone@gmail.com>
2012-07-07 21:35:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeadCreator::Application.config.secret_token = 'd754589c32426c7dd5c71e146ddec24169aea0357ad59f661addf91aeea44072bccb71260ffdd037c9465d1d72f46f15f6f5d4c1ec1bf387fc36b58d64ccb258'
